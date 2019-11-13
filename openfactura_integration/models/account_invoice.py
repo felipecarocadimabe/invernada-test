9168b7ff14cd8d10a86ad81dad3be9a91b6cdd64
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-13 15:36:50 -0300

    dte_type y dte_payment_mode a account_invoice

--- openfactura_integration/models/account_invoice.py
+++ openfactura_integration/models/account_invoice.py
@@ -4,5 +4,15 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    
+    dte_type_id = fields.Many2one(
+        'dte.type',
+        'Tipo de Factura',
+        required=True
+    )
+
+    dte_payment_mode_id = fields.Many2one(
+        'dte.payment.mode',
+        'Forma de Pago',
+        required=True
+    )
 
