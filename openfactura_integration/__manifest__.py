d32d0ecab84f64988ddbb3c6d16eb930430021e6
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-13 11:26:35 -0300

    se agrega res_partner.xml a manifest

--- openfactura_integration/__manifest__.py
+++ openfactura_integration/__manifest__.py
@@ -29,6 +29,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/acteco.xml',
+        'views/res_partner.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
