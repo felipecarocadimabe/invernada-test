2af1eb8da201a619f721f745eafa42324a8f7911
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-13 14:37:22 -0300

    campos requeridos en dte_type

--- openfactura_integration/models/dte_type.py
+++ openfactura_integration/models/dte_type.py
@@ -3,20 +3,31 @@ from odoo import models, fields, api
 
 class DTEType(models.Model):
     _name = 'dte.type'
+    _sql_constraints = [
+        ('unique_id', 'UNIQUE(code)', 'el código ya se encuentra en el sistema')
+    ]
 
     name = fields.Char(
         'Tipo',
         compute='_get_name',
-        store=True,
-        invisible=True
+        store=True
     )
 
-    code = fields.Char('Código')
+    code = fields.Char(
+        'Código',
+        required=True
+    )
 
-    dte_type = fields.Char('Tipo DTE')
+    dte_type = fields.Char(
+        'Tipo DTE',
+        required=True
+    )
 
     @api.multi
     @api.depends('code', 'dte_type')
     def _get_name(self):
         for item in self:
-            item.name = '{} {}'.format(item.code, item.dte_type)
+            if item.dte_type and item.code:
+                item.name = '{} {}'.format(item.code, item.dte_type)
+            else:
+                item.name = ''
