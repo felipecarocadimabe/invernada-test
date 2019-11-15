74a2e3f8dca7810ea7244e53ce4feb0219e28a8c
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-15 12:41:31 -0300

    cambio a compute

--- editable_currency/models/account_invoice.py
+++ editable_currency/models/account_invoice.py
@@ -7,18 +7,20 @@ class AccountInvoice(models.Model):
 
     exchange_rate = fields.Float(
         'Taza de Cambio',
-        default=lambda self: self._default_exchange_rate()
+        compute='_default_exchange_rate',
+        store=True
     )
 
     @api.model
+    @api.depends('date_invoice')
     def _default_exchange_rate(self):
         date = self.date_invoice
-        raise models.ValidationError(str(date))
-        if not date:
-            date = datetime.date.today()
-        currency_id = self.env['res.currency'].search([('name', '=', 'USD')])
-        rate = currency_id.rate_ids.search([('name', '=', date)])
-        rate.ensure_one()
-        return 1 / rate.rate
+        if date:
+            currency_id = self.env['res.currency'].search([('name', '=', 'USD')])
+            rate = currency_id.rate_ids.search([('name', '=', date)])
+            rate.ensure_one()
+            return 1 / rate.rate
+        else:
+            return 0
 
 
