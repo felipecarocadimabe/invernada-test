958dc1873e44e442d65d993a16273ed6058a1aee
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-15 11:00:42 -0300

    modulo editable_currency

---
+++ editable_currency/models/account_invoice.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    exchange_rate = fields.Float(
+        'Taza de Cambio',
+        default='_default_exchange_rate'
+    )
+
+    @api.model
+    def _default_exchange_rate(self):
+        return 700
+
