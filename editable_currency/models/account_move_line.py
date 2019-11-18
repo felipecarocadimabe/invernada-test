ce09f1c2022e26f8f58a0d8a0b48ee80e800a011
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-18 10:33:00 -0300

    debug

--- editable_currency/models/account_move_line.py
+++ editable_currency/models/account_move_line.py
@@ -8,5 +8,5 @@ class AccountMoveLine(models.Model):
 
         debit, credit, amount_currency, currency_id = super(AccountMoveLine, self)._compute_amount_fields(amount, src_currency, company_currency)
 
-        raise models.ValidationError('{} {} {} {}'.format(debit, credit, amount_currency, currency_id))
+        raise models.ValidationError('{} - {} - {} - {}'.format(debit, credit, amount_currency, currency_id))
 
--- editable_currency/models/res_currency.py
+++ editable_currency/models/res_currency.py
@@ -3,31 +3,3 @@ from odoo import models, fields
 
 class ResCurrency(models.Model):
     _inherit = 'res.currency'
-
-    def _convert(self, from_amount, to_currency, company, date, round=True, optional_usd=False):
-
-        """Returns the converted amount of ``from_amount``` from the currency
-           ``self`` to the currency ``to_currency`` for the given ``date`` and
-           company.
-
-           :param company: The company from which we retrieve the convertion rate
-           :param date: The nearest date from which we retriev the conversion rate.
-           :param round: Round the result or not
-        """
-        self, to_currency = self or to_currency, to_currency or self
-        assert self, "convert amount from unknown currency"
-        assert to_currency, "convert amount to unknown currency"
-        assert company, "convert amount from unknown company"
-        assert date, "convert amount from unknown date"
-        # apply conversion rate
-        if self == to_currency:
-            to_amount = from_amount
-        else:
-            exchange = self._get_conversion_rate(self, to_currency, company, date)
-
-            if optional_usd:
-                exchange = 1 / optional_usd
-
-            to_amount = from_amount * exchange
-        # apply rounding
-        return to_currency.round(to_amount) if round else to_amount
