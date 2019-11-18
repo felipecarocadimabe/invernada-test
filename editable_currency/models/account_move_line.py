3233af78429bebbb2b088c8791f37507168522c2
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-18 10:30:09 -0300

    debug

--- editable_currency/models/account_move_line.py
+++ editable_currency/models/account_move_line.py
@@ -5,23 +5,8 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     def _compute_amount_fields(self, amount, src_currency, company_currency):
-        """ Helper function to compute value for fields debit/credit/amount_currency based on an amount and the currencies given in parameter"""
-        amount_currency = False
-        currency_id = False
-        optional_usd = False
 
-        if self.invoice_id:
-            optional_usd = self.invoice_id.exchange_rate
-        else:
-            raise models.ValidationError(self.id)
+        debit, credit, amount_currency, currency_id = super(AccountMoveLine, self)._compute_amount_fields(amount, src_currency, company_currency)
 
-        date = self.env.context.get('date') or fields.Date.today()
-        company = self.env.context.get('company_id')
-        company = self.env['res.company'].browse(company) if company else self.env.user.company_id
-        if src_currency and src_currency != company_currency:
-            amount_currency = amount
-            amount = src_currency._convert(amount, company_currency, company, date, optional_usd)
-            currency_id = src_currency.id
-        debit = amount > 0 and amount or 0.0
-        credit = amount < 0 and -amount or 0.0
-        return debit, credit, amount_currency, currency_id
+        raise models.ValidationError('{} {} {} {}'.format(debit, credit, amount_currency, currency_id))
+
