09324ded8c7899df0a7e8a8576d38a97a86e4486
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-18 15:57:39 -0300

    debug

--- editable_currency/models/account_move_line.py
+++ editable_currency/models/account_move_line.py
@@ -22,25 +22,3 @@ class AccountMoveLine(models.Model):
         debit = amount > 0 and amount or 0.0
         credit = amount < 0 and -amount or 0.0
         return debit, credit, amount_currency, currency_id
-
-    def _check_reconcile_validity(self):
-        # Perform all checks on lines
-
-        company_ids = set()
-        all_accounts = []
-        for line in self:
-            company_ids.add(line.company_id.id)
-            all_accounts.append(line.account_id)
-            if (line.matched_debit_ids or line.matched_credit_ids) and line.reconciled:
-                raise models.ValidationError(_('You are trying to reconcile some entries that are already reconciled.'))
-        if len(company_ids) > 1:
-            raise models.ValidationError(_('To reconcile the entries company should be the same for all entries.'))
-        if len(set(all_accounts)) > 1:
-            raise models.ValidationError(all_accounts)
-            raise models.ValidationError(_('Entries are not from the same account.'))
-        if not (all_accounts[0].reconcile or all_accounts[0].internal_type == 'liquidity'):
-            raise models.ValidationError(_(
-                'Account %s (%s) does not allow reconciliation. First change the configuration of this account to allow it.') % (
-                                             all_accounts[0].name, all_accounts[0].code))
-
-
