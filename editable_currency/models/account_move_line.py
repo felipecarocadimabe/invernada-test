3ad717458e5a017f99f4200cf9e2b3944e8248b5
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-18 09:38:30 -0300

    debug

--- editable_currency/models/account_move_line.py
+++ editable_currency/models/account_move_line.py
@@ -14,7 +14,7 @@ class AccountMoveLine(models.Model):
         if self.invoice_id:
             optional_usd = self.invoice_id.exchange_rate
         else:
-            raise models.ValidationError(self)
+            raise models.ValidationError(self.invoice_id)
 
         date = self.env.context.get('date') or fields.Date.today()
         company = self.env.context.get('company_id')
