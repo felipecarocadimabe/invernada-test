2a83e9a306d194fe5d06a7f3e62b16663f1b694f
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-18 14:58:16 -0300

    debug

--- editable_currency/models/account_move_line.py
+++ editable_currency/models/account_move_line.py
@@ -6,7 +6,7 @@ class AccountMoveLine(models.Model):
 
     def _compute_amount_fields(self, amount, src_currency, company_currency):
 
-        raise models.ValidationError('{} _compute_amount_field'.format(self._context.get('optional_uds')))
+        raise models.ValidationError('{} _compute_amount_field'.format(self._context.get('date', False)))
 
         debit, credit, amount_currency, currency_id = super(AccountMoveLine, self)._compute_amount_fields(amount, src_currency, company_currency)
 
