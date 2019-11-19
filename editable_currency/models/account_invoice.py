47518fbd560172a33274087688356416b4be3198
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-19 17:18:50 -0300

    debug

--- editable_currency/models/account_invoice.py
+++ editable_currency/models/account_invoice.py
@@ -26,6 +26,8 @@ class AccountInvoice(models.Model):
 
     def action_invoice_open(self):
 
+
+
         if self.id:
             if not self.exchange_rate or self.exchange_rate == 0:
                 raise models.ValidationError('debe existir una taza de cambio')
@@ -33,3 +35,34 @@ class AccountInvoice(models.Model):
         return super(AccountInvoice, self).action_invoice_open()
 
 
+
+
+
+    @api.multi
+    def compute_invoice_totals(self, company_currency, invoice_move_lines):
+        raise models.ValidationError(self.exchange_rate)
+        total = 0
+        total_currency = 0
+        optional_usd = self.env.context.get('optional_usd') or False
+        for line in invoice_move_lines:
+            if self.currency_id != company_currency:
+                currency = self.currency_id
+                date = self._get_currency_rate_date() or fields.Date.context_today(self)
+                if not (line.get('currency_id') and line.get('amount_currency')):
+                    line['currency_id'] = currency.id
+                    line['amount_currency'] = currency.round(line['price'])
+                    line['price'] = currency.with_context(
+                        optional_usd=optional_usd
+                    )._convert(line['price'], company_currency, self.company_id, date)
+            else:
+                line['currency_id'] = False
+                line['amount_currency'] = False
+                line['price'] = self.currency_id.round(line['price'])
+            if self.type in ('out_invoice', 'in_refund'):
+                total += line['price']
+                total_currency += line['amount_currency'] or line['price']
+                line['price'] = - line['price']
+            else:
+                total -= line['price']
+                total_currency -= line['amount_currency'] or line['price']
+        return total, total_currency, invoice_move_lines
