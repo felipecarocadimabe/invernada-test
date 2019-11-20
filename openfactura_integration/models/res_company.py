1e913c4455eac9499d6334aca7f6ef494ae256da
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-20 15:24:50 -0300

    debug

--- openfactura_integration/models/res_company.py
+++ openfactura_integration/models/res_company.py
@@ -1,6 +1,7 @@
 from odoo import models, fields
 import requests
 import json
+import datetime
 
 
 class ResCompany(models.Model):
@@ -21,7 +22,22 @@ class ResCompany(models.Model):
 
         response = json.loads(res.text)
 
-        raise models.ValidationError(res.text)
-
-        if res.status_code != 200:
-            raise models.ValidationError(res.text)
+        if response['data']:
+            for dte in response['data']:
+                tmp = self.env['account.invoice'].search([('dte_folio', '=', dte['Folio'])])
+                if len(tmp) == 0:
+                    provider = self.env['res.partner'].search([('name', '=', dte['RznSoc'])])
+                    partner_id = None
+                    if len(provider) == 1:
+                        partner_id = provider.id
+                    self.env['account.invoice'].create({
+                        'dte_folio': dte['Folio'],
+                        'dte_type_id': dte['TipoDTE'],
+                        'dte_payment_mode_id': dte['FmaPago'],
+                        'date_invoice': datetime.strptime(dte['FchEmis'], '%Y-%m-%d'),
+                        'amount_untaxed': dte['MntNeto'],
+                        'amount_tax': dte['IVA'],
+                        'amount_total': dte['MntTotal'],
+                        'partner_id': partner_id
+                    })
+                    raise models.ValidationError(res.text)
