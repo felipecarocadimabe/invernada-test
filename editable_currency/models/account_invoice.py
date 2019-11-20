8bc6734b2ed753b91e6328df70540d1f0330da29
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-20 09:11:01 -0300

    debug

--- editable_currency/models/account_invoice.py
+++ editable_currency/models/account_invoice.py
@@ -16,6 +16,7 @@ class AccountInvoice(models.Model):
         if date:
             currency_id = self.env['res.currency'].search([('name', '=', 'USD')])
             rate = currency_id.rate_ids.search([('name', '=', date)])
+            raise models.ValidationError(rate)
             try:
                 rate.ensure_one()
                 self.exchange_rate = 1 / rate.rate
