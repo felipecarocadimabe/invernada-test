ccfa7003ed9f97d74546694cad79a2bc7d203549
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-21 10:55:17 -0300

    debug

--- openfactura_integration/models/res_company.py
+++ openfactura_integration/models/res_company.py
@@ -22,65 +22,67 @@ class ResCompany(models.Model):
 
         response = json.loads(res.text)
 
-        if response['data']:
-            for dte in response['data']:
-                tmp = self.env['account.invoice'].search([('dte_folio', '=', dte['Folio'])])
-                if len(tmp) == 0:
-                    provider = self.env['res.partner'].search([('name', '=', dte['RznSoc'])])
-                    dte_type = self.env['dte.type'].search([('code', '=', dte['TipoDTE'])])
-                    dte_payment_mode = self.env['dte.payment.mode'].search([('code', '=', dte['FmaPago'])])
-                    partner_id = None
-                    if len(provider) == 1:
-                        partner_id = provider.id
+        if res.status_code == 200:
 
-                    dv = calculate_dv(dte['RUTEmisor'])
-                    rut = '{}-{}'.format(dte['RUTEmisor'], dv)
+            if response['data']:
+                for dte in response['data']:
+                    tmp = self.env['account.invoice'].search([('dte_folio', '=', dte['Folio'])])
+                    if len(tmp) == 0:
+                        provider = self.env['res.partner'].search([('name', '=', dte['RznSoc'])])
+                        dte_type = self.env['dte.type'].search([('code', '=', dte['TipoDTE'])])
+                        dte_payment_mode = self.env['dte.payment.mode'].search([('code', '=', dte['FmaPago'])])
+                        partner_id = None
+                        if len(provider) == 1:
+                            partner_id = provider.id
 
-                    detail_res = requests.request(
-                        'GET',
-                        'https://dev-api.haulmer.com/v2/dte/document/{}/{}/{}/json'.format(
-                            rut,
-                            dte['TipoDTE'],
-                            dte['Folio']
-                        ),
-                        headers={
-                            'apikey': self.api_key
-                        }
-                    )
-                    detail_response = json.dumps(detail_res.text)
+                        dv = calculate_dv(dte['RUTEmisor'])
+                        rut = '{}-{}'.format(dte['RUTEmisor'], dv)
 
-                    raise models.ValidationError(detail_res.status_code)
-
-                    if detail_response['json']:
-
-                        invoice_lines = []
-
-                        for line in detail_response['json']['Detalle']:
-                            product = self.env['product.product'].search(['name', '=', line['NmbItem']])
-
-                            invoice_line = {
-                                'secuence': line['NroLinDet'],
-                                'quantity': line['QtyItem'],
-                                'price_unit': line['PrcItem'],
-                                'price_subtotal': line['MontoItem'],
-                                'product_id': product.id
+                        detail_res = requests.request(
+                            'GET',
+                            'https://dev-api.haulmer.com/v2/dte/document/{}/{}/{}/json'.format(
+                                rut,
+                                dte['TipoDTE'],
+                                dte['Folio']
+                            ),
+                            headers={
+                                'apikey': self.api_key
                             }
+                        )
+                        detail_response = json.dumps(detail_res.text)
 
-                            invoice_lines.append(invoice_line)
+                        if detail_res.status_code == 200:
 
-                        if len(invoice_lines) > 0:
+                            if detail_response['json']:
 
-                            self.env['account.invoice'].with_context(
-                                default_type='in_invoice',
-                                type='in_invoice'
-                            ).create({
-                                'dte_folio': dte['Folio'],
-                                'dte_type_id': dte_type.id,
-                                'dte_payment_mode_id': dte_payment_mode.id,
-                                'date_invoice': dte['FchEmis'],
-                                'amount_untaxed': dte['MntNeto'],
-                                'amount_tax': dte['IVA'],
-                                'amount_total': dte['MntTotal'],
-                                'partner_id': partner_id,
-                                'invoice_line_ids': invoice_lines
-                            })
+                                invoice_lines = []
+
+                                for line in detail_response['json']['Detalle']:
+                                    product = self.env['product.product'].search(['name', '=', line['NmbItem']])
+
+                                    invoice_line = {
+                                        'secuence': line['NroLinDet'],
+                                        'quantity': line['QtyItem'],
+                                        'price_unit': line['PrcItem'],
+                                        'price_subtotal': line['MontoItem'],
+                                        'product_id': product.id
+                                    }
+
+                                    invoice_lines.append(invoice_line)
+
+                                if len(invoice_lines) > 0:
+
+                                    self.env['account.invoice'].with_context(
+                                        default_type='in_invoice',
+                                        type='in_invoice'
+                                    ).create({
+                                        'dte_folio': dte['Folio'],
+                                        'dte_type_id': dte_type.id,
+                                        'dte_payment_mode_id': dte_payment_mode.id,
+                                        'date_invoice': dte['FchEmis'],
+                                        'amount_untaxed': dte['MntNeto'],
+                                        'amount_tax': dte['IVA'],
+                                        'amount_total': dte['MntTotal'],
+                                        'partner_id': partner_id,
+                                        'invoice_line_ids': invoice_lines
+                                    })
