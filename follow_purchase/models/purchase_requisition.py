6638a872d670aff203736c62057534ceccaf928b
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-12 08:36:49 -0300

    debug

--- follow_purchase/models/purchase_requisition.py
+++ follow_purchase/models/purchase_requisition.py
@@ -11,7 +11,7 @@ class PurchaseRequisition(models.Model):
         item = super(PurchaseRequisition, self).create(vals_list)
 
         if channel:
-            item.message_channel_ids.append((6, 0, channel.id))
+            item.message_channel_ids = [(6, 0, channel.id)]
 
         raise models.ValidationError(item.message_channel_ids)
 
